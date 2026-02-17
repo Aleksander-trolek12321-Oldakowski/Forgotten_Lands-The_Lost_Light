6a5ccb04c6e31e65e247e1cf3b9f369d9d70251c
AdrianN374 <anawrocki@edu.cdv.pl>
2026-02-17 07:42:05 +0100

    Drzewko (w trakcie)

---
+++ Assets/Scripts/Skille/SkillInfoPanel.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class SkillInfoPanel : MonoBehaviour
+{
+    public TextMeshProUGUI skillNameText;
+    public TextMeshProUGUI skillDescriptionText;
+    public Button unlockButton;
+
+    Skill currentSkill;
+    SkillTree skillTree;
+
+    public void Init(SkillTree tree)
+    {
+        skillTree = tree;
+        Clear();
+    }
+
+    public void ShowSkill(Skill skill, string description)
+    {
+        currentSkill = skill;
+
+        skillNameText.text = skill.skillName;
+        skillDescriptionText.text = description;
+
+        unlockButton.interactable = !skill.unlocked && skill.CanUnlock();
+    }
+
+    public void OnUnlockClicked()
+    {
+        if (currentSkill == null) return;
+
+        if (skillTree.TryUnlockSkill(currentSkill))
+        {
+            Debug.Log($"Unlocked skill: {currentSkill.skillName}");
+            unlockButton.interactable = false;
+        }
+    }
+
+    void Clear()
+    {
+        skillNameText.text = "";
+        skillDescriptionText.text = "";
+        unlockButton.interactable = false;
+    }
+}
